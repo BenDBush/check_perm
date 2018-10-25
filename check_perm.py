636082e1895624199b92fb0b879d5e6d84ef2f6b
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-25 13:07:45 -0400

    Gave up on regex, shifted to sticking text inbetween padding.

--- check_perm.py
+++ check_perm.py
@@ -4,7 +4,9 @@ from hypothesis import given, assume
 import hypothesis.strategies as st
 
 def check_is_perm(string1, string2):
-    """return True if string1 is a permutation of string2, False otherwise."""
+    """return True if string1 is a permutation of string2, False otherwise.
+    Treats escape sequences as the constituting characters,
+    so that \n is a permutation of n\a, etc.,"""
     for character in repr(string1):
         if character in repr(string2):
             string2= repr(string2).replace(character, '', 1)
@@ -16,26 +18,29 @@ def check_is_perm(string1, string2):
 
 
 class TestPerm(unittest.TestCase):
-    @given(st.text(), st.text())
-    def test_perm_smoke(self, str1, str2):
-        check_is_perm(str1, str2)
+#    @given(st.text(), st.text())
+#    def test_perm_smoke(self, str1, str2):
+#        check_is_perm(str1, str2)
 
 
     @given(st.from_regex(r"^(.+)(-)(?!\1)(.+)$"))  # this isn't right...
     def test_perm_false(self, test_str):
         str1, str2 = test_str.split('-', 1)
-        self.assertFalse(check_is_perm(str1, str2))  # fails when one is empty and one is white space.
+        self.assertFalse(check_is_perm(str1, str2))
 
 
-    @given(st.from_regex(r"^(.*)(-)(.*)(?=\1)(.*)"))
-    def test_perm_true(self, test_str):
-        str1, str2 = test_str.split('-', 1)
-        self.assertTrue(check_is_perm(str1, str2))
+    @given(st.text(), st.text(), st.text())  # gives memory errors.
+    def test_perm_true(self, test_str, pad_l, pad_r):
+        string_permed = pad_l + test_str + pad_r
+        self.assertTrue(check_is_perm(test_str, string_permed))
 
+
+    @given(st.text(), st.text(), st.text())
+    def test_perm_true_reversed(self, test_str, pad_l, pad_r):
+        string_permed = str(pad_l) + test_str[::-1] + str(pad_r)
+        self.assertTrue(check_is_perm(test_str, string_permed))
 
 
         
 if __name__ == "__main__":
-#    unittest.main()
-    if check_is_perm(' ', '\n'):
-        print("True")
+    unittest.main()
