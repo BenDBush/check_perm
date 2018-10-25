8afb7f189635a840cf472d34c268e0de5ff896c3
Benjamin Bush <benjamin.bush15@houghton.edu>
2018-10-25 09:56:09 -0400

    Dealt with escape characters. Having issues with regex.

--- check_perm.py
+++ check_perm.py
@@ -5,9 +5,9 @@ import hypothesis.strategies as st
 
 def check_is_perm(string1, string2):
     """return True if string1 is a permutation of string2, False otherwise."""
-    for character in string1:
-        if character in string2:
-            string2.replace(character, '', 1)
+    for character in repr(string1):
+        if character in repr(string2):
+            string2= repr(string2).replace(character, '', 1)
         else:
             return False
 
@@ -21,20 +21,21 @@ class TestPerm(unittest.TestCase):
         check_is_perm(str1, str2)
 
 
-    @given(st.from_regex(r"^(.*)(-)(?![\1])(.*)$"))
+    @given(st.from_regex(r"^(.+)(-)(?!\1)(.+)$"))  # this isn't right...
     def test_perm_false(self, test_str):
-        assume(len(test_str) > 1)
-        str1, str2 = test_str.split('-', 1)  # need to pass these as raw strings throughout...
+        str1, str2 = test_str.split('-', 1)
         self.assertFalse(check_is_perm(str1, str2))  # fails when one is empty and one is white space.
 
 
-    @given(st.from_regex(r"^(.*)(-)(.*)(?:[\1])(.*)"))
+    @given(st.from_regex(r"^(.*)(-)(.*)(?=\1)(.*)"))
     def test_perm_true(self, test_str):
-        str1, str2 = test_str.split('-', 1)  # see comment above--also, any way to reduce code reuse?
-        self.assertTrue(check_is_perm(str1, str2))  # failing with test_str = '0-\x01' b/c '\x01' is a bytes object.
+        str1, str2 = test_str.split('-', 1)
+        self.assertTrue(check_is_perm(str1, str2))
 
 
 
         
 if __name__ == "__main__":
-    unittest.main()
+#    unittest.main()
+    if check_is_perm(' ', '\n'):
+        print("True")
